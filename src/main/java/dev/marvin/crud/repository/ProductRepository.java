dc24fb4438e497e3def609244cb4f0056cff7964
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2023-09-01 08:24:17 -0600

    Add repositories

---
+++ src/main/java/dev/marvin/crud/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package dev.marvin.crud.repository;
+
+import dev.marvin.crud.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    Optional<Product> findByName(String name);
+    boolean existsByName(String name);
+}
---
+++ src/main/java/dev/marvin/crud/security/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package dev.marvin.crud.security.repository;
+
+import dev.marvin.crud.security.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+}
