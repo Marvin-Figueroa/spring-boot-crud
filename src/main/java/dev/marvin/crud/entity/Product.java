583755da559e217a68bdc397218688e7f3653b62
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2023-08-31 20:04:07 -0600

    Add Product entity

---
+++ src/main/java/dev/marvin/crud/entity/Product.java
@@ -0,0 +1,27 @@
+package dev.marvin.crud.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
