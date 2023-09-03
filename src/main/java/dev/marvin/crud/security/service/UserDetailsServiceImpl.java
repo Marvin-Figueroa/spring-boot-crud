333b69dbf1fe69af515a1d6f286c4d4934759c0c
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2023-09-03 10:02:18 -0600

    Add UserDetailsService implementation

---
+++ src/main/java/dev/marvin/crud/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package dev.marvin.crud.security.service;
+
+import dev.marvin.crud.security.entity.MainUser;
+import dev.marvin.crud.security.entity.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.getByUsername(username).get();
+        return MainUser.build(user);
+    }
+}
